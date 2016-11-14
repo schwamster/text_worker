f71f6a052ce4b48dd451210c212d3832012b2f96
schwamster <bastian.toepfer@bodoconsult.de>
2016-11-14 21:32:33 +0100

    added configuration support config file + environment vars

--- text_worker/Program.cs
+++ text_worker/Program.cs
@@ -7,6 +7,10 @@ using System.Net;
 using System.Net.Sockets;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using System.Net.Http;
+using System.Globalization;
+using System.IO;
 
 namespace text_worker
 {
@@ -14,19 +18,29 @@ namespace text_worker
     {
         public static int Main(string[] args)
         {
+            ConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
+            configurationBuilder
+                .AddJsonFile("appsettings.json", false)
+                .AddEnvironmentVariables();
+
+            var configuration = configurationBuilder.Build();
+
+            string queue = configuration["Queue"];
             try
             {
                 var redis = OpenRedisConnection("redis").GetDatabase();
 
-                var definition = new { name = "", size = 0, user="", client="", content="" };
-
+                var definition = new { name = "", size = 0, user = "", client = "", content = "" };
+                Console.WriteLine($"Starting to read from Queue: {queue}");
                 while (true)
                 {
-                    string json = redis.ListLeftPopAsync("documents:process:1").Result;
+                    string json = redis.ListLeftPopAsync(queue).Result;
                     if (json != null)
                     {
                         var document = JsonConvert.DeserializeAnonymousType(json, definition);
                         Console.WriteLine($"Processing document '{document.name}' uploaded by '{document.user}/{document.client}'");
+                        var result = ExtractText(document.name, document.content, configuration["OcrServiceHost"]);
+                        Console.WriteLine($"Result from ocr: {result}");
                     }
                     else
                     {
@@ -40,6 +54,28 @@ namespace text_worker
                 return 1;
             }
         }
+
+        private static async Task<string> ExtractText(string name, string content, string ocrServiceHost)
+        {
+            //make call to ocr_service
+            byte[] document = System.Convert.FromBase64String(content);
+            using (var client = new HttpClient())
+            {
+                using (var msg = new MultipartFormDataContent("Upload----" + DateTime.Now.ToString(CultureInfo.InvariantCulture)))
+                {
+                    msg.Add(new StreamContent(new MemoryStream(document)), "files", name);
+                    var url = $"http://{ocrServiceHost}/api/ocr";
+                    using (var message = await client.PostAsync(url, msg))
+                    {
+                        var input = await message.Content.ReadAsStringAsync();
+                        Console.WriteLine($"Document sent to {url}");
+                        return input;
+                    }
+                }
+            }
+        }
+
+
 
         private static ConnectionMultiplexer OpenRedisConnection(string hostname)
         {
