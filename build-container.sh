b9517e2f90ddc12e1486aab7cef1c329496ba218
schwamster <bastian.toepfer@bodoconsult.de>
2016-11-27 21:04:18 +0100

    wrong dir

--- build-container.sh
+++ build-container.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 set -e
 dotnet restore
-dotnet test test/text-worker.test/project.json
+dotnet test test/text_worker.test/project.json
 rm -rf $(pwd)/publish/worker
 dotnet publish src/text_worker/project.json -c release -o $(pwd)/publish/worker
